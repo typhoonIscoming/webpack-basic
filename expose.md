572ccea1308fa152fa51fc655b63f8c8195bed2a
xiehang <850525344@qq.com>
2019-02-15 18:10:21 +0800

    expose-loader暴露全局变量的插件及使用

--- expose.md
+++ expose.md
@@ -3,3 +3,5 @@
 - 在文件中引入插件 如： import $ from 'juqery'
 - console.log(window.$)  // undefine
 - 此时就需要用到expose-loader。它是专门将插件暴露到全局中
+- npm i expose-loader
+- 使用 如引入jquery: import $ from 'expose-loader?$!jquery'
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 var tool = require('./a')
 require('./index.css')
 require('./index.less')
+import $ from 'expose-loader?$!jquery'
 
 let fn = () => {
     console.log('this is es6 syntax')
@@ -21,3 +22,5 @@ fn()
 // console.log('this is class A', a.a)
 console.log('this is my first webpack prictise')
 console.log('9 plus 2 equal', tool.add(9,2))
+
+console.log($, window.$)
